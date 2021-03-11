540ec9aaafae80a5fc6883c08525513d7e7129ad
Анастасия Войтенко <anastasiavojtenko81471@gmail.com>
2021-03-11 22:44:05 +0300

    Added methods

--- src/main/java/tasks/first/FirstTaskSolution.java
+++ src/main/java/tasks/first/FirstTaskSolution.java
@@ -1,18 +1,96 @@
 package tasks.first;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+
 public class FirstTaskSolution implements FirstTask {
     @Override
     public String breadthFirst(boolean[][] adjacencyMatrix, int startIndex) {
-        return null;
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        boolean[] isVisited = new boolean[adjacencyMatrix.length];
+        deque.push(startIndex);
+        isVisited[startIndex] = true;
+        ArrayList<Integer> elements = new ArrayList<>();
+        while (!deque.isEmpty()) {
+            elements.add(deque.getFirst());
+            int firstElement = deque.getFirst();
+            deque.removeFirst();
+            for (int i = 0; i < adjacencyMatrix.length; i++) {
+                if(adjacencyMatrix[firstElement][i] && !isVisited[i]){
+                    deque.add(i);
+                    isVisited[i] = true;
+                }
+            }
+        }
+        return elements.toString();
     }
 
     @Override
     public Boolean validateBrackets(String s) {
-        return null;
+
+        String openBracket = "([{";
+        String closedBracket = ")]}";
+
+        char[] input = s.toCharArray();
+        Deque<Character> deque = new ArrayDeque<>();
+        for (Character letter: input){
+            if (openBracket.contains(letter.toString())){
+                deque.push(letter);
+            }
+            if (closedBracket.contains(letter.toString())){
+
+                if (deque.isEmpty()){
+                    return false;
+                }
+
+                if(closedBracket.indexOf(letter) != openBracket.indexOf(deque.pop())){
+                    return false;
+                }
+            }
+        }
+        return deque.isEmpty();
     }
 
     @Override
-    public Long polishCalculation(String s) {
-        return null;
+    public Long polishCalculation(String s) throws IllegalArgumentException {
+        String[] numbers = s.split(" ");
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        long result = 0;
+        int first;
+        int second;
+        try {
+            for (String num :numbers) {
+                deque.push(Integer.parseInt(num));
+            }
+        }
+        catch (IllegalArgumentException e){
+            for (int i = deque.size(); i < numbers.length ; i++) {
+                first = deque.peek();
+                deque.pop();
+                second = deque.peek();
+                deque.pop();
+                switch (numbers[i]){
+                    case "-":
+                        deque.push(first - second);
+                        break;
+                    case  "+":
+                        deque.push(first + second);
+                        break;
+                    case "*":
+                        deque.push(first * second);
+                        break;
+                    case "/":
+                        deque.push(first / second);
+                        break;
+
+                    default:
+                        throw new IllegalArgumentException("Unsupported value: " + numbers[i]);
+
+                }
+                result = deque.getFirst();
+            }
+        }
+        return result;
     }
 }
